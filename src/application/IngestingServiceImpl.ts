657c296457b31b22584ce3ee297c81d6015bd5ff
rares vasiliu <rv.vasiliu@gmail.com>
2025-11-28 16:49:53 +0100

    feat: add IngestingService implementation

---
+++ src/application/IngestingServiceImpl.ts
@@ -0,0 +1,56 @@
+import { IngestingService } from "@domain/ports/IngestingService";
+import { MonitoringRepository } from "@domain/ports/MonitoringRepository";
+import { SmartFurnitureHookupService } from "./ports/SmartFurnitureHookupService";
+import { HouseholdUserService } from "./ports/HouseholdUserService";
+import { SmartFurnitureHookupID } from "@domain/SmartFurnitureHookupID";
+import { HouseholdUserUsername } from "@domain/HouseholdUserUsername";
+import { InvalidSmartFurnitureHookupIDError } from "@domain/errors";
+import { MeasurementFactory } from "@domain/MeasurementFactory";
+
+export class IngestingServiceImpl implements IngestingService {
+  constructor(
+    private readonly monitoringRepository: MonitoringRepository,
+    private readonly smartFurnitureHookupService: SmartFurnitureHookupService,
+    private readonly householdUserService: HouseholdUserService,
+  ) {}
+
+  async createMeasurement(
+    smartFurnitureHookupID: SmartFurnitureHookupID,
+    consumptionValue: number,
+    timestamp: Date,
+    householdUserUsername?: HouseholdUserUsername,
+  ): Promise<void> {
+    const smartFurnitureHookup =
+      await this.smartFurnitureHookupService.getSmartFurnitureHookup(
+        smartFurnitureHookupID,
+      );
+
+    if (!smartFurnitureHookup) {
+      throw new InvalidSmartFurnitureHookupIDError();
+    }
+
+    return await this.monitoringRepository.saveMeasurement(
+      new MeasurementFactory().createMeasurement(
+        smartFurnitureHookupID,
+        smartFurnitureHookup.utilityType,
+        consumptionValue,
+        isNaN(timestamp.getTime()) ? new Date() : timestamp,
+
+        await this.getValidHouseHoldUsername(householdUserUsername),
+      ),
+    );
+  }
+
+  private async getValidHouseHoldUsername(
+    username?: HouseholdUserUsername,
+  ): Promise<HouseholdUserUsername | undefined> {
+    if (!username) {
+      return undefined;
+    }
+
+    const isValid =
+      await this.householdUserService.isHouseholdUserUsernameValid(username);
+
+    return isValid ? username : undefined;
+  }
+}
