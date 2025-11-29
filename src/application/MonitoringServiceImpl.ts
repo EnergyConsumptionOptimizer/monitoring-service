fc39e4df922e71028906e42ff8e315a48e9354ac
rares vasiliu <rv.vasiliu@gmail.com>
2025-11-29 17:53:17 +0100

    feat: add MonitoringService implementation

---
+++ src/application/MonitoringServiceImpl.ts
@@ -0,0 +1,36 @@
+import { MonitoringService } from "@domain/ports/MonitoringService";
+import { MonitoringRepository } from "@domain/ports/MonitoringRepository";
+import { SmartFurnitureHookupID } from "@domain/SmartFurnitureHookupID";
+import { UtilityMeters } from "@domain/UtilityMeters";
+import { TimeRangeFilter } from "@domain/utils/TimeRangeFilter";
+import { TagsFilter } from "@domain/utils/TagsFilter";
+import { UtilityType } from "@domain/UtilityType";
+import { TimeSeriesFilter } from "@domain/utils/TimeSeriesFilter";
+import { ConsumptionPoint } from "@domain/ConsumptionPoint";
+
+export class MonitoringServiceImpl implements MonitoringService {
+  constructor(private readonly monitoringRepository: MonitoringRepository) {}
+
+  async getActiveSmartFurnitureHookups(): Promise<SmartFurnitureHookupID[]> {
+    return this.monitoringRepository.findActiveSmartFurnitureHookups();
+  }
+
+  async getUtilityMeters(
+    filter?: TimeRangeFilter,
+    tagsFilter?: TagsFilter,
+  ): Promise<UtilityMeters> {
+    return this.monitoringRepository.findUtilityMeters(filter, tagsFilter);
+  }
+
+  async getUtilityConsumptions(
+    utilityType: UtilityType,
+    filter?: TimeSeriesFilter,
+    tagsFilter?: TagsFilter,
+  ): Promise<ConsumptionPoint[]> {
+    return this.monitoringRepository.findUtilityConsumptions(
+      utilityType,
+      filter,
+      tagsFilter,
+    );
+  }
+}
