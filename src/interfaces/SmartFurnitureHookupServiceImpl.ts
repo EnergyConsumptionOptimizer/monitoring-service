c155474031f9b86bc026f9b0dbaf269d7f78ce0f
rares vasiliu <rv.vasiliu@gmail.com>
2025-11-29 11:44:05 +0100

    feat: add SmartFurnitureHookupService implementation

---
+++ src/interfaces/SmartFurnitureHookupServiceImpl.ts
@@ -0,0 +1,28 @@
+import { SmartFurnitureHookupService } from "@application/ports/SmartFurnitureHookupService";
+import { SmartFurnitureHookupID } from "@domain/SmartFurnitureHookupID";
+import { SmartFurnitureHookup } from "@application/SmartFurnitureHookup";
+import axios from "axios";
+import { utilityTypeFromString } from "@domain/UtilityType";
+
+export class SmartFurnitureHookupServiceImpl
+  implements SmartFurnitureHookupService
+{
+  constructor(private readonly baseUrl: string) {}
+
+  async getSmartFurnitureHookup(
+    smartFurnitureHookupID: SmartFurnitureHookupID,
+  ): Promise<SmartFurnitureHookup | null> {
+    const url = `${this.baseUrl}/internal/smart-furniture-hookups/${smartFurnitureHookupID.value()}`;
+
+    try {
+      const response = await axios.get(url);
+
+      return {
+        id: response.data.id,
+        utilityType: utilityTypeFromString(response.data.utilityType),
+      };
+    } catch {
+      return null;
+    }
+  }
+}
