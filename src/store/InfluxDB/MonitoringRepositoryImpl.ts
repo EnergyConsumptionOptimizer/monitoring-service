935c24ebe2aeb0ff8fb0f11d3d7bb35c781c1b4e
rares vasiliu <rv.vasiliu@gmail.com>
2025-11-30 11:53:26 +0100

    feat: add findUtilityConsumptions implementation to MonitoringRepositoryImpl

--- src/store/InfluxDB/MonitoringRepositoryImpl.ts
+++ src/store/InfluxDB/MonitoringRepositoryImpl.ts
@@ -5,9 +5,13 @@ import { Point } from "@influxdata/influxdb-client";
 import { TimeRangeFilter } from "@domain/utils/TimeRangeFilter";
 import { TagsFilter } from "@domain/utils/TagsFilter";
 import { UtilityMeters } from "@domain/UtilityMeters";
-import { utilityTypeFromString } from "@domain/UtilityType";
-import { RawUtilityMeters } from "./RawUtilityMeters";
+import { UtilityType, utilityTypeFromString } from "@domain/UtilityType";
+import { UtilityMetersModel } from "./models/UtilityMetersModel";
 import { UtilityMetersQueryBuilder } from "./query/UtilityMetersQueryBuilder";
+import { TimeSeriesFilter } from "@domain/utils/TimeSeriesFilter";
+import { ConsumptionPoint } from "@domain/ConsumptionPoint";
+import { ConsumptionSeriesQueryBuilder } from "./query/ConsumptionSeriesQueryBuilder";
+import { ConsumptionPointModel } from "./models/ConsumptionPointModel";
 
 export class MonitoringRepositoryImpl {
   constructor(private readonly influxDB: InfluxDBClient) {}
@@ -40,17 +44,43 @@ export class MonitoringRepositoryImpl {
       .withUser(tagsFilter?.username)
       .build();
 
-    const result: RawUtilityMeters[] = await this.influxDB.queryAsync(query);
+    const result: UtilityMetersModel[] = await this.influxDB.queryAsync(query);
 
     return result.reduce((acc, row) => {
       try {
         const utilityType = utilityTypeFromString(row._measurement);
         acc[utilityType] = row._value;
-      } catch (_err) {
-        console.error(_err);
+      } catch {
+        console.error("Invalid utility measurement: ", row._measurement);
       }
 
       return acc;
     }, {} as UtilityMeters);
+  }
+
+  async findUtilityConsumptions(
+    utilityType: UtilityType,
+    filter?: TimeSeriesFilter,
+    tagsFilter?: TagsFilter,
+  ): Promise<ConsumptionPoint[]> {
+    const query = ConsumptionSeriesQueryBuilder.forBucket(
+      this.influxDB.getBucket(),
+    )
+      .withUtility(utilityType)
+      .withStart(filter?.from)
+      .withStop(filter?.to)
+      .withUser(tagsFilter?.username)
+      .withWindow(filter?.granularity)
+      .build();
+
+    console.log(query);
+
+    const result: ConsumptionPointModel[] =
+      await this.influxDB.queryAsync(query);
+
+    return result.map((point) => ({
+      value: point._value,
+      timestamp: point._time,
+    }));
   }
 }
