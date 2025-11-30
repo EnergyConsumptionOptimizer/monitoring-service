d621fef8f4b2b2be305b1241448c0faea4832839
rares vasiliu <rv.vasiliu@gmail.com>
2025-11-30 22:38:23 +0100

    feat: add monitoring web-api endpoints in the router

--- src/interfaces/web-api/routes/routes.ts
+++ src/interfaces/web-api/routes/routes.ts
@@ -1,12 +1,21 @@
 import { IngestingController } from "../controllers/IngestingController";
 import { Router } from "express";
+import { MonitoringController } from "@interfaces/web-api/controllers/MonitoringController";
 
-export function router(ingestingController: IngestingController): Router {
+export function router(
+  ingestingController: IngestingController,
+  monitoringController: MonitoringController,
+): Router {
   const router = Router();
 
   router.post(
     "/api/internal/measurements",
     ingestingController.createMeasurement,
+  );
+
+  router.get(
+    "/api/internal/measurements/:utilityType",
+    monitoringController.getUtilityConsumptions,
   );
 
   return router;
