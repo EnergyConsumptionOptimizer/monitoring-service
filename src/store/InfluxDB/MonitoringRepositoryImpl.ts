ac5b1b3adbea59aa2b4170b7ff010e8a4344985c
rares vasiliu <rv.vasiliu@gmail.com>
2025-11-30 01:24:45 +0100

    feat: add findUtilityMeters method into MonitoringRepositoryImpl

--- src/store/InfluxDB/MonitoringRepositoryImpl.ts
+++ src/store/InfluxDB/MonitoringRepositoryImpl.ts
@@ -2,6 +2,12 @@ import { InfluxDBClient } from "./InfluxDBClient";
 import { Measurement } from "@domain/Measurement";
 import { MeasurementTag } from "./MeasurementTag";
 import { Point } from "@influxdata/influxdb-client";
+import { TimeRangeFilter } from "@domain/utils/TimeRangeFilter";
+import { TagsFilter } from "@domain/utils/TagsFilter";
+import { UtilityMeters } from "@domain/UtilityMeters";
+import { utilityTypeFromString } from "@domain/UtilityType";
+import { RawUtilityMeters } from "./RawUtilityMeters";
+import { UtilityMetersQueryBuilder } from "./query/UtilityMetersQueryBuilder";
 
 export class MonitoringRepositoryImpl {
   constructor(private readonly influxDB: InfluxDBClient) {}
@@ -22,5 +28,29 @@ export class MonitoringRepositoryImpl {
       );
 
     await this.influxDB.writePoint(point);
+  }
+
+  async findUtilityMeters(
+    filter?: TimeRangeFilter,
+    tagsFilter?: TagsFilter,
+  ): Promise<UtilityMeters> {
+    const query = UtilityMetersQueryBuilder.forBucket(this.influxDB.getBucket())
+      .withStart(filter?.from)
+      .withStop(filter?.to)
+      .withUser(tagsFilter?.username)
+      .build();
+
+    const result: RawUtilityMeters[] = await this.influxDB.queryAsync(query);
+
+    return result.reduce((acc, row) => {
+      try {
+        const utilityType = utilityTypeFromString(row._measurement);
+        acc[utilityType] = row._value;
+      } catch (_err) {
+        console.error(_err);
+      }
+
+      return acc;
+    }, {} as UtilityMeters);
   }
 }
