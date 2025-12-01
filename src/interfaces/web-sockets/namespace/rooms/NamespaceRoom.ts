7eb8614c41e5b072550c57d3e994d6710686ad8e
rares vasiliu <rv.vasiliu@gmail.com>
2025-12-02 00:18:54 +0100

    refactor: add name propriety to NamespaceRoom

--- src/interfaces/web-sockets/namespace/rooms/NamespaceRoom.ts
+++ src/interfaces/web-sockets/namespace/rooms/NamespaceRoom.ts
@@ -1,6 +1,7 @@
 import { Namespace, Socket } from "socket.io";
 
 export interface NamespaceRoom {
+  name(): string;
   setup(namespace: Namespace): void;
   subscribe(socket: Socket): void;
   unsubscribe(socket: Socket): void;
