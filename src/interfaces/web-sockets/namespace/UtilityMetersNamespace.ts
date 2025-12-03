e94140afe69608529f42851eb34aafe88bdda6ab
rares vasiliu <rv.vasiliu@gmail.com>
2025-12-04 00:03:15 +0100

    feat: add utility meters namespace to get custom utility meters with periodic updates with custom update time

---
+++ src/interfaces/web-sockets/namespace/UtilityMetersNamespace.ts
@@ -0,0 +1,55 @@
+import { SocketNamespace } from "@interfaces/web-sockets/SocketNamespace";
+import { Namespace, Server } from "socket.io";
+import { UtilityMetersSubscription } from "@interfaces/web-sockets/namespace/subscriptions/UtilityMetersSubscription";
+import { UtilityMetersSocket } from "@interfaces/web-sockets/sockets/UtilityMetersSocket";
+import { UtilityMetersQueryDTO } from "@presentation/web-socket/UtilityMetersQueryDTO";
+
+export class UtilityMetersNamespace implements SocketNamespace {
+  private readonly NAMESPACE_PATH = "/utility-meters";
+  private namespace?: Namespace;
+
+  constructor(private utilityMetersSubscription: UtilityMetersSubscription) {}
+
+  name(): string {
+    return this.NAMESPACE_PATH;
+  }
+
+  setup(io: Server): void {
+    this.namespace = io.of(this.name());
+
+    this.namespace.on("connect", (socket: UtilityMetersSocket) => {
+      this.handleConnection(socket);
+      this.handleEditQuery(socket);
+
+      socket.on("disconnect", () =>
+        this.utilityMetersSubscription.unsubscribe(socket),
+      );
+    });
+  }
+
+  private handleConnection(socket: UtilityMetersSocket) {
+    socket.on(
+      "subscribe",
+      (queries: UtilityMetersQueryDTO[], interval?: number) =>
+        this.utilityMetersSubscription.subscribe(socket, queries, interval),
+    );
+  }
+
+  private handleEditQuery(socket: UtilityMetersSocket) {
+    socket.on("editQuery", (query: UtilityMetersQueryDTO) =>
+      this.utilityMetersSubscription.addOrEditQuery(socket, query),
+    );
+  }
+
+  stop(io: Server): void {
+    io._nsps.delete(this.name());
+    this.disconnectAllClients();
+  }
+
+  private disconnectAllClients(): void {
+    this.namespace?.sockets.forEach((socket) => {
+      this.utilityMetersSubscription.unsubscribe(socket);
+      socket.disconnect(true);
+    });
+  }
+}
---
+++ src/interfaces/web-sockets/namespace/subscriptions/UtilityMetersSubscription.ts
@@ -0,0 +1,116 @@
+import { ClientSocketLock } from "@interfaces/web-sockets/ClientSocketLock";
+import { PeriodicSubscription } from "@interfaces/web-sockets/PeriodicSubscription";
+import { UtilityMetersHandler } from "@interfaces/web-sockets/handlers/UtilityMetersHandler";
+import { UtilityMetersSocket } from "@interfaces/web-sockets/sockets/UtilityMetersSocket";
+import { UtilityMetersQueryDTO } from "@presentation/web-socket/UtilityMetersQueryDTO";
+import { UtilityMetersQueryResultMapper } from "@presentation/web-socket/UtilityMetersQueryResultDTO";
+
+export class UtilityMetersSubscription {
+  private periodicSubscription: PeriodicSubscription =
+    new PeriodicSubscription();
+  private clientsQueries = new Map<string, UtilityMetersQueryDTO[]>();
+
+  private lock: ClientSocketLock = new ClientSocketLock();
+
+  constructor(private readonly utilityMetersHandler: UtilityMetersHandler) {}
+
+  async subscribe(
+    socket: UtilityMetersSocket,
+    queries: UtilityMetersQueryDTO[],
+    interval?: number,
+  ) {
+    if (!queries || queries.length === 0) {
+      console.log("No queries found.");
+      socket.emit("error", "At least one query must be provided");
+      return;
+    }
+
+    this.clientsQueries.set(socket.id, queries);
+
+    try {
+      await this.sendUtilityMetersUpdate(socket);
+    } catch (error) {
+      socket.emit("error", `Initial update failed: ${error}`);
+      this.clientsQueries.delete(socket.id);
+      return;
+    }
+
+    this.periodicSubscription.newSubscription(
+      socket,
+      async () => {
+        await this.sendUtilityMetersUpdate(socket);
+      },
+      interval,
+    );
+  }
+
+  unsubscribe(socket: UtilityMetersSocket) {
+    this.periodicSubscription.unsubscribeSocket(socket);
+    this.clientsQueries.delete(socket.id);
+  }
+
+  async addOrEditQuery(
+    socket: UtilityMetersSocket,
+    query: UtilityMetersQueryDTO,
+  ) {
+    const queries = this.clientsQueries.get(socket.id);
+
+    if (!queries) {
+      socket.emit("error", "No active subscription found");
+      return;
+    }
+
+    const release = await this.lock.acquire(socket.id);
+
+    try {
+      const queryIndex = queries?.findIndex((q) => q.label === query.label);
+
+      if (queryIndex != -1) {
+        queries.splice(queryIndex, 1);
+      }
+
+      const update = await this.getUtilityMetersQueryResult(query);
+
+      socket.emit("utilityMetersQueryUpdate", update);
+
+      queries.push(query);
+
+      this.clientsQueries.set(socket.id, queries);
+    } finally {
+      release();
+    }
+  }
+
+  private async sendUtilityMetersUpdate(
+    socket: UtilityMetersSocket,
+  ): Promise<void> {
+    const release = await this.lock.acquire(socket.id);
+
+    try {
+      const queries = this.clientsQueries.get(socket.id);
+
+      if (!queries) {
+        return;
+      }
+      const updates = await Promise.all(
+        queries.map(async (query) => this.getUtilityMetersQueryResult(query)),
+      );
+
+      socket.emit("utilityMetersUpdate", updates);
+    } catch (error) {
+      socket.emit("error", `Failed to fetch utility consumptions: ${error}`);
+      console.error(`Error for ${socket.id}:`, error);
+    } finally {
+      release();
+    }
+  }
+
+  private async getUtilityMetersQueryResult(query: UtilityMetersQueryDTO) {
+    const utilityMetersData = await this.utilityMetersHandler.getUtilityMeters(
+      query.filter,
+      query.tagFilter,
+    );
+
+    return UtilityMetersQueryResultMapper.toDTO(query.label, utilityMetersData);
+  }
+}
