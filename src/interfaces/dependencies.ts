961c2b3a19760188c680b56952e667e237833495
Vasiliu Rares <rv.vasiliu@gmail.com>
2026-01-20 11:14:15 +0100

    fix: remove api form services uri

--- src/interfaces/dependencies.ts
+++ src/interfaces/dependencies.ts
@@ -37,16 +37,16 @@ export const monitoringRepositoryImpl = new MonitoringRepositoryImpl(
 );
 
 // ===== Services =====
-const smartFurnitureHookupServiceUri = `http://${process.env.HOOKUP_SERVICE_HOST ?? "smart-furniture-hookup-service"}:${process.env.HOOKUP_SERVICE_PORT ?? "3002"}/api`;
+const smartFurnitureHookupServiceUri = `http://${process.env.HOOKUP_SERVICE_HOST ?? "smart-furniture-hookup-service"}:${process.env.HOOKUP_SERVICE_PORT ?? "3002"}`;
 export const smartFurnitureHookupServiceImpl =
   new SmartFurnitureHookupServiceImpl(smartFurnitureHookupServiceUri);
 
-const userServiceUri = `http://${process.env.USER_SERVICE_HOST ?? "user-service"}:${process.env.USER_SERVICE_PORT ?? "3000"}/api`;
+const userServiceUri = `http://${process.env.USER_SERVICE_HOST ?? "user-service"}:${process.env.USER_SERVICE_PORT ?? "3000"}`;
 export const householdUserServiceImpl = new HouseholdUserServiceImpl(
   userServiceUri,
 );
 
-const mapServiceUri = `http://${process.env.MAP_SERVICE_HOST ?? "map-service"}:${process.env.MAP_SERVICE_PORT ?? "3001"}/api`;
+const mapServiceUri = `http://${process.env.MAP_SERVICE_HOST ?? "map-service"}:${process.env.MAP_SERVICE_PORT ?? "3001"}`;
 export const mapServiceImpl = new MapServiceImpl(mapServiceUri);
 
 export const ingestingServiceImpl = new IngestingServiceImpl(
