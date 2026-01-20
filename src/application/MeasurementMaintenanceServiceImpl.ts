3079fa8ea07f2eb7782501ebee7a327c6b5f5d0b
Vasiliu Rares <rv.vasiliu@gmail.com>
2026-01-20 11:13:41 +0100

    feat: implement removeZoneIDTagFromMeasurements in MeasurementMaintenanceServiceImpl

--- src/application/MeasurementMaintenanceServiceImpl.ts
+++ src/application/MeasurementMaintenanceServiceImpl.ts
@@ -1,6 +1,7 @@
 import { MeasurementMaintenanceService } from "@domain/ports/MeasurementMaintenanceService";
 import { MonitoringRepository } from "@domain/ports/MonitoringRepository";
 import { HouseholdUserUsername } from "@domain/HouseholdUserUsername";
+import { ZoneID } from "@domain/ZoneID";
 
 export class MeasurementMaintenanceServiceImpl implements MeasurementMaintenanceService {
   constructor(private readonly monitoringRepository: MonitoringRepository) {}
@@ -11,5 +12,9 @@ export class MeasurementMaintenanceServiceImpl implements MeasurementMaintenance
     return this.monitoringRepository.deleteHouseholdUserTagFromMeasurements(
       username,
     );
+  }
+
+  removeZoneIDTagFromMeasurements(zoneID: ZoneID): Promise<void> {
+    return this.monitoringRepository.deleteZoneIDTagFromMeasurements(zoneID);
   }
 }
